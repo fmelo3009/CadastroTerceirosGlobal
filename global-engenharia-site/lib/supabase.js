91941a17ac6d849e49d53e6130ae0f3674426ff6
Fernando Melo <fmelo3009@gmail.com>
2026-09-08 12:52:47 -0400

    Create supabase.js

---
+++ global-engenharia-site/lib/supabase.js
@@ -0,0 +1,37 @@
+const {
+  createClient
+} = require(
+  '@supabase/supabase-js'
+);
+
+const supabaseUrl =
+  process.env.SUPABASE_URL;
+
+const supabaseServiceKey =
+  process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+
+if (
+  !supabaseUrl ||
+  !supabaseServiceKey
+) {
+  throw new Error(
+    'SUPABASE_URL ou SUPABASE_SERVICE_ROLE_KEY não configurada.'
+  );
+}
+
+
+const supabase =
+  createClient(
+    supabaseUrl,
+    supabaseServiceKey,
+    {
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false
+      }
+    }
+  );
+
+
+module.exports = supabase;
